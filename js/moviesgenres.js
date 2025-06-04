32dc75c815341e7a09a1ff267af0deb0670604b1
mgarciapoitevin <mgarciapoitevin@udesa.edu.ar>
2025-06-04 17:26:08 -0300

    paginas seriesgenres.js y moviesgenres.js ya estan, faltan hacer un par de cambios en css para que se vean mejor

--- js/moviesgenres.js
+++ js/moviesgenres.js
@@ -1,1 +1,23 @@
-console.log("funciona?") 
+const movies_genres = document.querySelector("#movies-genres")
+let generos = ""
+
+fetch("https://api.themoviedb.org/3/genre/movie/list?language=es&api_key=2c3f31bcc0252bb3b9edba5fa7b52869")
+    .then( function(response){
+        return response.json()
+    })
+    .then( function(data){
+        
+        for (i = 0; i < data.genres.length; i++){ 
+            generos += `
+                <article class="peliculahome">
+                    <a href="./detail-movie-genres.html?${data.genres[i].name}">
+                        <h4>${data.genres[i].name}</h4>
+                    </a>
+                </article>
+            `
+        }
+        movies_genres.innerHTML = generos
+    })
+    .catch( function(error){
+        console.log(error)
+    })
