2ae479ec6e8fb52a96f09756ad5d8108b66b2068
claramonzo1 <cmonzo@udesa.edu.ar>
2025-06-04 23:49:13 -0300

    Details genero serie js hecho, las pelis se ven mal

--- js/detail-series-genres.js
+++ js/detail-series-genres.js
@@ -1,16 +1,46 @@
-let queryString = location.search
-let queryStringObj = new URLSearchParams(queryString)
-let id_genero = queryStringObj.get("genero")
+// Obtener el id del género desde la URL
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let id_genero = queryStringObj.get("genero");
 
-let series_genero = document.querySelector(".secciones")
+// Capturar el lugar donde mostrar las series
+let series_genero = document.querySelector(".secciones");
+let tituloGenero = document.querySelector(".titulo-genero");
 
-fetch(`https://api.themoviedb.org/3/discover/tv?&language=es-ES&with_genres=${id_genero}&api_key=2c3f31bcc0252bb3b9edba5fa7b52869`)
-    .then(
+// Hacer la consulta a la API de series por género
+fetch(`https://api.themoviedb.org/3/discover/tv?language=es-ES&with_genres=${id_genero}&api_key=2c3f31bcc0252bb3b9edba5fa7b52869`)
+  .then(function(respuesta) {
+    return respuesta.json(); // Convertir la respuesta a JSON
+  })
+  .then(function(datos) {
+    // Cambiar el título
+    tituloGenero.innerText = "Series del género";
 
-    )
-    .then(
+    // Si no hay resultados
+    if (datos.results.length === 0) {
+      series_genero.innerHTML = "<p>No se encontraron series para este género.</p>";
+    } else {
+      // Si hay resultados, los recorro
+      let contenido = "";
 
-    )
-    .catch(
+      for (let i = 0; i < datos.results.length; i++) {
+        let serie = datos.results[i];
 
-    )
+        contenido += `
+          <article class="peliculahome">
+            <a href="./detail-serie.html?id=${serie.id}">
+              <img src="https://image.tmdb.org/t/p/w300${serie.poster_path}" alt="${serie.name}">
+              <h4>${serie.name}</h4>
+              <p>Fecha de estreno: ${serie.first_air_date}</p>
+            </a>
+          </article>
+        `;
+      }
+
+      // Insertar el contenido en la página
+      series_genero.innerHTML = contenido;
+    }
+  })
+  .catch(function(error) {
+    console.log("Ocurrió un error al cargar las series:", error);
+  });
